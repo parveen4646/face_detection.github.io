9e4b21648c2f47d63bc6762bdc06705b75267d0c
Parveen Sharma <parveen133012@gmail.com>
2024-03-04 21:17:39 +0530

    ads

--- face.py
+++ face.py
@@ -1,24 +1,23 @@
-
 from mtcnn import MTCNN
 import os
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
-import keras
 import torch
+
 from facenet_pytorch import InceptionResnetV1
 
-
+# Removing import of keras, as it's not used in the provided code
 
 def import_files():
     list_of_images=os.listdir('/Users/Guchi/Documents/projects/face detection model/images')
-    print('filesimported')
+    print('files imported')
     print(len(list_of_images))
-    return  list_of_images
+    return list_of_images
 
 
 def extract_face(filename, required_size=(224, 224), extracted_images_dic1={}):
-    filename_='images/'+str(filename)
+    filename_ = 'images/' + str(filename)
     # load image from file
     pixels = plt.imread(filename_)
     # create the detector, using default weights
@@ -27,36 +26,30 @@ def extract_face(filename, required_size=(224, 224), extracted_images_dic1={}):
     results = detector.detect_faces(pixels)
     # extract the bounding box from the first face
     extracted_images=[]
-    for i in range(0,len(results)):
+    for i in range(0, len(results)):
         x1, y1, width, height = results[i]['box']
         x2, y2 = x1 + width, y1 + height
         face = pixels[y1:y2, x1:x2]
-        image=cv2.resize(face,required_size)
+        image = cv2.resize(face, required_size)
         extracted_images.append(image)
-        extracted_images_dic={filename:extracted_images}
+        extracted_images_dic = {filename: extracted_images}
         extracted_images_dic1.update(extracted_images_dic)
     return extracted_images_dic1
 
 
-
-
-def generate_embedding(pixels,model):
-    emb_list=[]
-    name_list=[]
-    for v,i in pixels.items():
+def generate_embedding(pixels, model):
+    emb_list = []
+    name_list = []
+    for v, i in pixels.items():
         for sep in i:
-            face=torch.tensor(sep)
-            face=(face/255.0-0.5)/.5
-            face=face.unsqueeze(0)
+            face = torch.tensor(sep)
+            face = (face / 255.0 - 0.5) / .5
+            face = face.unsqueeze(0)
             tensor_permuted = face.permute(0, 3, 1, 2)
-            embeddings=model(tensor_permuted)
+            embeddings = model(tensor_permuted)
             emb_list.append(embeddings)
             name_list.append(v)
-    return emb_list,name_list
-
-
-
-
+    return emb_list, name_list
 
 
 def stack_embed(embeddings):
@@ -65,9 +58,10 @@ def stack_embed(embeddings):
 
 
 from sklearn.metrics.pairwise import cosine_similarity
-import pandas as pd 
+import pandas as pd
 
-def fina_result(final_embeddings,list_names):
+
+def fina_result(final_embeddings, list_names):
     print('final loop')
     print(os.getcwd())
     result_indices = []
@@ -76,32 +70,31 @@ def fina_result(final_embeddings,list_names):
         score = torch.nn.functional.cosine_similarity(final_embeddings[i], final_embeddings)
         indices = np.where(score > 0.8)[0]  # np.where returns a tuple, so we access the first element ([0])
         result_indices.append(indices)
-    xx=pd.Series([list(i) for i in result_indices if len(i)>1]).drop_duplicates().reset_index(drop=True)
+    xx = pd.Series([list(i) for i in result_indices if len(i) > 1]).drop_duplicates().reset_index(drop=True)
     print(xx)
-    for m,i in enumerate(xx):
-        os.makedirs(str(m),exist_ok=True)
+    for m, i in enumerate(xx):
+        os.makedirs(str(m), exist_ok=True)
         for j in i:
-            k=list_names[j]
-            l=f'./images/{k}'
-            imagee=plt.imread(l)
-            plt.imsave(f'./{m}/{k}',imagee)
+            k = list_names[j]
+            l = f'./images/{k}'
+            imagee = plt.imread(l)
+            plt.imsave(f'./{m}/{k}', imagee)
             print('result save')
-    
-    
+
 
 def main():
-    list_of_images=import_files()
-    images_dict={i:file for i,file in enumerate(list_of_images)}
+    list_of_images = import_files()
+    images_dict = {i: file for i, file in enumerate(list_of_images)}
 
     global model
-    model=InceptionResnetV1(pretrained='vggface2').eval()
-    imc=list(map(extract_face,list_of_images))
-    imc=imc[0]
-    embeddings,list_names=generate_embedding(imc,model)
-    embeddd=stack_embed(embeddings)
-    final_embeddings=embeddd
-    fina_result(final_embeddings,list_names)
+    model = InceptionResnetV1(pretrained='vggface2', classify=False).eval()
+    imc = list(map(extract_face, list_of_images))
+    imc = imc[0]
+    embeddings, list_names = generate_embedding(imc, model)
+    embeddd = stack_embed(embeddings)
+    final_embeddings = embeddd
+    fina_result(final_embeddings, list_names)
 
 
- 
-
+if __name__ == "__main__":
+    main()
