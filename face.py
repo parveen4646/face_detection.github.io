d1ed51b58783791932234d3be5022599a39698de
Parveen Sharma <parveen133012@gmail.com>
2024-03-06 10:27:13 +0530

    adding google

--- face.py
+++ face.py
@@ -9,50 +9,60 @@ from facenet_pytorch import InceptionResnetV1
 
 # Removing import of keras, as it's not used in the provided code
 
-def import_files():
-    list_of_images=os.listdir('/Users/Guchi/Documents/projects/face detection model/images')
+def import_files(folder_path):
+    list_of_images=os.listdir(folder_path)
+    #full_paths = [os.path.join(folder_path, filename) for filename in list_of_images]
+    # Remove unwanted prefixes from the file paths
+    print(f'folder{folder_path}')
+    print(f'directory{os.getcwd()}')
+    print(os.listdir(os.getcwd()))
     print('files imported')
+    print(list_of_images)
     print(len(list_of_images))
     return list_of_images
 
 
 def extract_face(filename, required_size=(224, 224), extracted_images_dic1={}):
-    filename_ = 'images/' + str(filename)
-    # load image from file
-    pixels = plt.imread(filename_)
-    # create the detector, using default weights
-    detector = MTCNN()
-    # detect faces in the image
-    results = detector.detect_faces(pixels)
-    # extract the bounding box from the first face
-    extracted_images=[]
-    for i in range(0, len(results)):
-        x1, y1, width, height = results[i]['box']
-        x2, y2 = x1 + width, y1 + height
-        face = pixels[y1:y2, x1:x2]
-        image = cv2.resize(face, required_size)
-        extracted_images.append(image)
-        extracted_images_dic = {filename: extracted_images}
-        extracted_images_dic1.update(extracted_images_dic)
+    filename_ = './images/'+ filename
+    if filename_.endswith((".jpg", ".png",".jpeg")):
+        # load image from file
+        pixels = plt.imread(filename_)
+        # create the detector, using default weights
+        detector = MTCNN()
+        # detect faces in the image
+        results = detector.detect_faces(pixels)
+        # extract the bounding box from the first face
+        extracted_images=[]
+        for i in range(0, len(results)):
+            x1, y1, width, height = results[i]['box']
+            x2, y2 = x1 + width, y1 + height
+            face = pixels[y1:y2, x1:x2]
+            image = cv2.resize(face, required_size)
+            extracted_images.append(image)
+            extracted_images_dic = {filename: extracted_images}
+            extracted_images_dic1.update(extracted_images_dic)
     return extracted_images_dic1
-
+    
+        
 
 def generate_embedding(pixels, model):
     emb_list = []
     name_list = []
-    for v, i in pixels.items():
-        for sep in i:
-            face = torch.tensor(sep)
-            face = (face / 255.0 - 0.5) / .5
-            face = face.unsqueeze(0)
-            tensor_permuted = face.permute(0, 3, 1, 2)
-            embeddings = model(tensor_permuted)
-            emb_list.append(embeddings)
-            name_list.append(v)
+    if pixels is not None:
+        for v, i in pixels.items():
+            for sep in i:
+                face = torch.tensor(sep)
+                face = (face / 255.0 - 0.5) / .5
+                face = face.unsqueeze(0)
+                tensor_permuted = face.permute(0, 3, 1, 2)
+                embeddings = model(tensor_permuted)
+                emb_list.append(embeddings)
+                name_list.append(v)
     return emb_list, name_list
 
 
 def stack_embed(embeddings):
+    embeddings = [i for i in embeddings if i is not None and len(i) > 0]
     stacked_embeddings = torch.cat(embeddings, dim=0)
     return stacked_embeddings
 
@@ -73,12 +83,14 @@ def fina_result(final_embeddings, list_names):
     xx = pd.Series([list(i) for i in result_indices if len(i) > 1]).drop_duplicates().reset_index(drop=True)
     print(xx)
     for m, i in enumerate(xx):
-        os.makedirs(str(m), exist_ok=True)
+        
         for j in i:
+            os.makedirs('images/'+str(m), exist_ok=True)
             k = list_names[j]
-            l = f'./images/{k}'
-            imagee = plt.imread(l)
-            plt.imsave(f'./{m}/{k}', imagee)
+            imagee = plt.imread(f'./images/{k}')
+            plt.imsave(f'./images/{str(m)}/{k}', imagee)
+       
+            print(f'its ={m}')
             print('result save')
 
 
